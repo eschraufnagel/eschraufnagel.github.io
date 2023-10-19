45558bc263164949d50418f2dcd8a5b6c6ec407a
Eric Schraufnagel <eschraufnagel@gmail.com>
2023-10-19 07:16:27 -0600

    Move GA to app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from "next/script";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-K78PJ85DCL`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-K78PJ85DCL');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from "next/script";
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}');
-        `}
-      </Script>
       <body>
         <Main />
         <NextScript />
